27f40e5ea44533733e5386bd652bbdedb4b9aa72
Basavaraju, Nuthan Kumar <nuthan.basavaraju@ge.com>
2021-03-22 16:31:25 +0000

    push first change

---
+++ controller.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+        opkit "github.com/rook/operator-kit"
+        sample "cosmosdb/pkg/apis/dbprovision/v1"
+        sampleclient "cosmosdb/pkg/client/clientset/versioned/typed/dbprovision/v1"
+        "k8s.io/client-go/kubernetes"
+        "k8s.io/client-go/tools/cache"
+)
+
+type SampleController struct {
+        context         *opkit.Context
+        sampleClientset sampleclient.DbprovisionV1Interface
+        clientset       *kubernetes.Clientset
+}
+
+func newSampleController(context *opkit.Context, sampleClientset sampleclient.DbprovisionV1Interface, clientset *kubernetes.Clientset) *SampleController {
+        return &SampleController{
+                context:         context,
+                sampleClientset: sampleClientset,
+                clientset:       clientset,
+        }
+}
+
+func (c *SampleController) StartWatch(namespace string, stopCh chan struct{}) error {
+
+        resourceHandlers := cache.ResourceEventHandlerFuncs{
+                AddFunc:    c.onAdd,
+                UpdateFunc: c.onUpdate,
+                DeleteFunc: c.onDelete,
+        }
+        restClient := c.sampleClientset.RESTClient()
+        watcher := opkit.NewWatcher(sample.CosmosResource, namespace, resourceHandlers, restClient)
+        go watcher.Watch(&sample.DBProvisioning{}, stopCh)
+        return nil
+}
+
+func (c *SampleController) onAdd(obj interface{}) {
+}
+
+func (c *SampleController) onUpdate(oldObj, newObj interface{}) {
+}
+
+func (c *SampleController) onDelete(obj interface{}) {
+
+}
+
