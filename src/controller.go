c74414d2b2a599ab0197c4f027474a027b69bad3
root <root@G6WDTNF2E.logon.ds.ge.com>
2021-03-24 16:46:08 +0530

    cosmos crud

--- src/controller.go
+++ src/controller.go
@@ -1,47 +1,85 @@
 package main
 
 import (
-        opkit "github.com/rook/operator-kit"
-        sample "github.com/nuthankumar/cosmosdb/pkg/apis/dbprovision/v1"
-        sampleclient "github.com/nuthankumar/cosmosdb/pkg/client/clientset/versioned/typed/dbprovision/v1"
-        "k8s.io/client-go/kubernetes"
-        "k8s.io/client-go/tools/cache"
+	"log"
+	"os"
+
+	"github.com/nuthankumar/az_cosmos_cassandra/operations"
+	"github.com/nuthankumar/az_cosmos_cassandra/utils"
+
+	sample "github.com/nuthankumar/cosmosdb/pkg/apis/dbprovision/v1"
+	sampleclient "github.com/nuthankumar/cosmosdb/pkg/client/clientset/versioned/typed/dbprovision/v1"
+	opkit "github.com/rook/operator-kit"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
+)
+
+var (
+	cosmosCassandraContactPoint string
+	cosmosCassandraPort         string
+	cosmosCassandraUser         string
+	cosmosCassandraPassword     string
 )
 
 type SampleController struct {
-        context         *opkit.Context
-        sampleClientset sampleclient.DbprovisionV1Interface
-        clientset       *kubernetes.Clientset
+	context         *opkit.Context
+	sampleClientset sampleclient.DbprovisionV1Interface
+	clientset       *kubernetes.Clientset
 }
 
+func init() {
+	cosmosCassandraContactPoint = os.Getenv("COSMOSDB_CASSANDRA_CONTACT_POINT")
+	cosmosCassandraPort = os.Getenv("COSMOSDB_CASSANDRA_PORT")
+	cosmosCassandraUser = os.Getenv("COSMOSDB_CASSANDRA_USER")
+	cosmosCassandraPassword = os.Getenv("COSMOSDB_CASSANDRA_PASSWORD")
+
+	if cosmosCassandraContactPoint == "" || cosmosCassandraUser == "" || cosmosCassandraPassword == "" {
+		log.Fatal("missing mandatory environment variables")
+	}
+}
 func newSampleController(context *opkit.Context, sampleClientset sampleclient.DbprovisionV1Interface, clientset *kubernetes.Clientset) *SampleController {
-        return &SampleController{
-                context:         context,
-                sampleClientset: sampleClientset,
-                clientset:       clientset,
-        }
+	return &SampleController{
+		context:         context,
+		sampleClientset: sampleClientset,
+		clientset:       clientset,
+	}
 }
 
 func (c *SampleController) StartWatch(namespace string, stopCh chan struct{}) error {
 
-        resourceHandlers := cache.ResourceEventHandlerFuncs{
-                AddFunc:    c.onAdd,
-                UpdateFunc: c.onUpdate,
-                DeleteFunc: c.onDelete,
-        }
-        restClient := c.sampleClientset.RESTClient()
-        watcher := opkit.NewWatcher(sample.CosmosResource, namespace, resourceHandlers, restClient)
-        go watcher.Watch(&sample.DBProvisioning{}, stopCh)
-        return nil
+	resourceHandlers := cache.ResourceEventHandlerFuncs{
+		AddFunc:    c.onAdd,
+		UpdateFunc: c.onUpdate,
+		DeleteFunc: c.onDelete,
+	}
+	restClient := c.sampleClientset.RESTClient()
+	watcher := opkit.NewWatcher(sample.CosmosResource, namespace, resourceHandlers, restClient)
+	go watcher.Watch(&sample.DBProvisioning{}, stopCh)
+	return nil
 }
 
 func (c *SampleController) onAdd(obj interface{}) {
+	data := obj.(*sample.DBProvisioning).DeepCopy()
+	session := utils.GetSession(cosmosCassandraContactPoint, cosmosCassandraPort, cosmosCassandraUser, cosmosCassandraPassword)
+	defer session.Close()
+	log.Println("Connected to Azure Cosmos DB")
+	operations.CreateKeySpace(data.Spec.Keyspacename, session)
 }
 
 func (c *SampleController) onUpdate(oldObj, newObj interface{}) {
+	data := oldObj.(*sample.DBProvisioning).DeepCopy()
+	newdata := newObj.(*sample.DBProvisioning).DeepCopy()
+	session := utils.GetSession(cosmosCassandraContactPoint, cosmosCassandraPort, cosmosCassandraUser, cosmosCassandraPassword)
+	defer session.Close()
+	log.Println("Connected to Azure Cosmos DB")
+	operations.DropKeySpaceIfExists(data.Spec.Keyspacename, session)
+	operations.CreateKeySpace(newdata.Spec.Keyspacename, session)
 }
 
 func (c *SampleController) onDelete(obj interface{}) {
-
+	data := obj.(*sample.DBProvisioning).DeepCopy()
+	session := utils.GetSession(cosmosCassandraContactPoint, cosmosCassandraPort, cosmosCassandraUser, cosmosCassandraPassword)
+	defer session.Close()
+	log.Println("Connected to Azure Cosmos DB")
+	operations.DropKeySpaceIfExists(data.Spec.Keyspacename, session)
 }
-
--- src/main.go
+++ src/main.go
@@ -18,113 +18,112 @@ limitations under the License.
 package main
 
 import (
-        "flag"
-        "fmt"
-        "os"
-        "os/signal"
-        "syscall"
-        "time"
-        opkit "github.com/rook/operator-kit"
-        sample "github.com/nuthankumar/cosmosdb/pkg/apis/dbprovision/v1"
-        sampleclient "github.com/nuthankumar/cosmosdb/pkg/client/clientset/versioned/typed/dbprovision/v1"
-        "k8s.io/api/core/v1"
-        apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-        "k8s.io/client-go/kubernetes"
-        "k8s.io/client-go/rest"
-        "k8s.io/client-go/tools/clientcmd"
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	sample "github.com/nuthankumar/cosmosdb/pkg/apis/dbprovision/v1"
+	sampleclient "github.com/nuthankumar/cosmosdb/pkg/client/clientset/versioned/typed/dbprovision/v1"
+	opkit "github.com/rook/operator-kit"
+	v1 "k8s.io/api/core/v1"
+	apiextensionsclient "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 var (
-        kubeconfig string
+	kubeconfig string
 )
 
 func init() {
-        flag.StringVar(&kubeconfig, "kubeconfig", "/opt/ees/kubespray/inventory/ees/artifacts/admin.conf", "Path to KUBECONFIG for running out of cluster. (Default: null)")
+	flag.StringVar(&kubeconfig, "kubeconfig", "/mnt/c/Users/212770199/.kube/config", "Path to KUBECONFIG for running out of cluster. (Default: null)")
 }
 
 func main() {
-        flag.Parse()
-        fmt.Println("Getting kubernetes context")
-        context, sampleClientset, clientset, err := createContext(kubeconfig)
-        if err != nil {
-                fmt.Println("failed to create context")
-                fmt.Printf("failed to create context. %+v\n", err)
-                os.Exit(1)
-        }
+	flag.Parse()
+	fmt.Println("Getting kubernetes context")
+	context, sampleClientset, clientset, err := createContext(kubeconfig)
+	if err != nil {
+		fmt.Println("failed to create context")
+		fmt.Printf("failed to create context. %+v\n", err)
+		os.Exit(1)
+	}
 
-        // Create and wait for CRD resources
-        fmt.Println("Registering the sample resource")
-        resources := []opkit.CustomResource{sample.CosmosResource}
-        err = opkit.CreateCustomResources(*context, resources)
-        if err != nil {
-                fmt.Println("Failed to create custom resource")
-                fmt.Printf("failed to create custom resource. %+v\n", err)
-                os.Exit(1)
-        }
+	// Create and wait for CRD resources
+	fmt.Println("Registering the sample resource")
+	resources := []opkit.CustomResource{sample.CosmosResource}
+	err = opkit.CreateCustomResources(*context, resources)
+	if err != nil {
+		fmt.Println("Failed to create custom resource")
+		fmt.Printf("failed to create custom resource. %+v\n", err)
+		os.Exit(1)
+	}
 
-        // create signals to stop watching the resources
-        signalChan := make(chan os.Signal, 1)
-        stopChan := make(chan struct{})
-        signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+	// create signals to stop watching the resources
+	signalChan := make(chan os.Signal, 1)
+	stopChan := make(chan struct{})
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
 
-        // start watching the sample resource
-        fmt.Println("Watching the sample resource")
-        fmt.Println(sampleClientset)
-        controller := newSampleController(context, sampleClientset, clientset)
-        controller.StartWatch(v1.NamespaceAll, stopChan)
+	// start watching the sample resource
+	fmt.Println("Watching the sample resource")
+	fmt.Println(sampleClientset)
+	controller := newSampleController(context, sampleClientset, clientset)
+	controller.StartWatch(v1.NamespaceAll, stopChan)
 
-        for {
-                select {
-                case <-signalChan:
-                        fmt.Println("shutdown signal received, exiting...")
-                        close(stopChan)
-                        return
-                }
-        }
+	for {
+		select {
+		case <-signalChan:
+			fmt.Println("shutdown signal received, exiting...")
+			close(stopChan)
+			return
+		}
+	}
 }
 
 func getClientConfig(kubeconfig string) (*rest.Config, error) {
-        if kubeconfig != "" {
-                fmt.Println("Getting kubeconfig from local")
-                return clientcmd.BuildConfigFromFlags("", kubeconfig)
-        }
-        fmt.Println("Getting Incluster Kubeconfig")
-        return rest.InClusterConfig()
+	if kubeconfig != "" {
+		fmt.Println("Getting kubeconfig from local")
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	fmt.Println("Getting Incluster Kubeconfig")
+	return rest.InClusterConfig()
 }
 
-func createContext(kubeconfig string) (*opkit.Context, sampleclient.DbprovisionV1Interface, *kubernetes.Clientset,   error) {
-        config, err := getClientConfig(kubeconfig)
-        if err != nil {
-                fmt.Println("failed to get k8s config")
-                return nil, nil, nil,fmt.Errorf("failed to get k8s config. %+v", err)
-        }
+func createContext(kubeconfig string) (*opkit.Context, sampleclient.DbprovisionV1Interface, *kubernetes.Clientset, error) {
+	config, err := getClientConfig(kubeconfig)
+	if err != nil {
+		fmt.Println("failed to get k8s config")
+		return nil, nil, nil, fmt.Errorf("failed to get k8s config. %+v", err)
+	}
 
-        clientset, err := kubernetes.NewForConfig(config)
-        if err != nil {
-                fmt.Println("failed to get k8s client")
-                return nil, nil,nil, fmt.Errorf("failed to get k8s client. %+v", err)
-        }
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println("failed to get k8s client")
+		return nil, nil, nil, fmt.Errorf("failed to get k8s client. %+v", err)
+	}
 
-        apiExtClientset, err := apiextensionsclient.NewForConfig(config)
-        if err != nil {
-                fmt.Println("failed to create k8s API extension clientset")
-                return nil, nil,nil, fmt.Errorf("failed to create k8s API extension clientset. %+v", err)
-        }
+	apiExtClientset, err := apiextensionsclient.NewForConfig(config)
+	if err != nil {
+		fmt.Println("failed to create k8s API extension clientset")
+		return nil, nil, nil, fmt.Errorf("failed to create k8s API extension clientset. %+v", err)
+	}
 
-        sampleClientset, err := sampleclient.NewForConfig(config)
-        if err != nil {
-                fmt.Println("failed to create sample clientset")
-                return nil, nil,nil, fmt.Errorf("failed to create sample clientset. %+v", err)
-        }
+	sampleClientset, err := sampleclient.NewForConfig(config)
+	if err != nil {
+		fmt.Println("failed to create sample clientset")
+		return nil, nil, nil, fmt.Errorf("failed to create sample clientset. %+v", err)
+	}
 
-        context := &opkit.Context{
-                Clientset:             clientset,
-                APIExtensionClientset: apiExtClientset,
-                Interval:              500 * time.Millisecond,
-                Timeout:               60 * time.Second,
-        }
-        return context, sampleClientset, clientset, nil
+	context := &opkit.Context{
+		Clientset:             clientset,
+		APIExtensionClientset: apiExtClientset,
+		Interval:              500 * time.Millisecond,
+		Timeout:               60 * time.Second,
+	}
+	return context, sampleClientset, clientset, nil
 
 }
-
-
